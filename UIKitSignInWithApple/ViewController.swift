9a0ccf27664c32843fbcd16c466c21f0c8271591
Alex Nagy <therebeloper@gmail.com>
2019-11-04 22:03:22 +0200

    Initial Commit

---
+++ UIKitSignInWithApple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitSignInWithApple
+//
+//  Created by Alex Nagy on 04/11/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
